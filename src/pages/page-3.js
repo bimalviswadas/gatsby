bdf303ebd6115584a677d7fd17b84ccba319f0a2
Bimal Viswadas <bviswadas@zaizi.com>
2020-04-27 14:27:13 +0100

    added gatsby-source-drupal

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page three" />
-    <h1>London grid for Learning</h1>
+    <h1>London Learning</h1>
     <p>Welcome to page 3</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
